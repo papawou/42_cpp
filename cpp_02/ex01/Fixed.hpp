3aaf3db33da1f2a9034155f79fc4f585621cbde6
Kevin Mendes <kmendes@student.42lausanne.ch>
2022-12-07 17:29:33 +0100

    fixed:

--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -7,6 +7,11 @@ public:
 	int	getRawBits(void) const;
 	void	setRawBits(int const raw);
 	
+	float toFloat(void) const;
+	int	toInt(void) const;
+
+	Fixed(int const nb);
+	Fixed(float const nb);
 	//colpien
 	Fixed(void);
 	Fixed(Fixed const &);
