7ac09eb33cd5a632a3415f543d7e2b6589ee015e
Kevin Mendes <kmendes@student.42lausanne.ch>
2023-01-11 19:31:16 +0100

    dsadsa

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -3,11 +3,14 @@
 
 int main()
 {
-	Span sp(10);
-	std::vector<int> v(3);
+	Span sp(6);
+	std::vector<int> v;
 	v.push_back(1);
 	v.push_back(2);
 	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+	v.push_back(6);
 
 	sp.insert(v.begin(), v.end());
 	std::cout << sp << std::endl;
