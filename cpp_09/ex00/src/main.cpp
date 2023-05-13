d34410c68eeaad2a251e4a5a19e0da24cf917454
Kevin Mendes <kmendes@student.42lausanne.ch>
2023-05-13 12:46:35 +0200

    look getline

--- cpp_09/ex00/src/main.cpp
+++ cpp_09/ex00/src/main.cpp
@@ -4,7 +4,6 @@
 #include <sstream>
 #include <map>
 
-
 int read_file(std::string const &file_path, std::string &str)
 {
 	std::ifstream ifs(file_path);
@@ -20,78 +19,223 @@ int read_file(std::string const &file_path, std::string &str)
 	return (0);
 }
 
-void format_db(std::string const &str, bool &date_first)
+// common
+std::string parse_date(std::string const &date)
+{
+	size_t i = 0;
+
+	if (date.empty())
+		throw("DATE not found");
+	i = 0;
+	for (; isdigit(date[i]); ++i)
+	{
+	}
+	if (i != 4 || date[i] != '-')
+		throw("DATE expected year");
+	++i;
+	for (; isdigit(date[i]); ++i)
+	{
+	}
+	if (i != 7 || date[i] != '-')
+		throw("DATE expected month");
+	if (date.substr(5, 2).compare("01") < 0 || date.substr(5, 2).compare("12") > 0)
+		throw("DATE invalid month");
+	++i;
+	for (; isdigit(date[i]); ++i)
+	{
+	}
+	if (i != 10 || date[i] != '\0')
+		throw("DATE expected day");
+	if (date.substr(8, 2).compare("00") <= 0 || date.substr(8, 2).compare("31") > 0) // check feb 30 31
+		throw("DATE invalid day");
+	return date;
+}
+
+float parse_value(std::string const &value)
+{
+	float f;
+	std::istringstream is(value);
+
+	if (value.empty())
+		throw("Number not found");
+	is >> f;
+	if (is.fail())
+		throw("Number conversion issue");
+	return f;
+}
+
+void parse_line(size_t &i, std::string const &delim, std::string const &str, std::string &first, std::string &second)
+{
+	size_t pos_delim;
+
+	first.clear();
+	second.clear();
+	pos_delim = str.find(delim, i);
+	if (pos_delim == std::string::npos)
+		throw("line delimiter not found");
+	for (; i != pos_delim; ++i)
+		first.push_back(str[i]);
+	i += delim.length();
+	for (; str[i] && str[i] != '\n'; ++i)
+		second.push_back(str[i]);
+}
+
+// DB
+void db_add_pair(std::string const &date, std::string const &value, std::map<std::string, float> &map)
+{
+	std::string date_map = parse_date(date);
+	float f = parse_value(value);
+
+	std::pair<std::map<std::string, float>::const_iterator, bool> ret;
+	ret = map.insert(std::pair<std::string, float>(date_map, f));
+	if (!ret.second)
+		throw("map duplicate key");
+}
+
+void db_parse(bool date_first, std::string const &str, std::map<std::string, float> &map)
+{
+	std::string first;
+	std::string second;
+	size_t i = 0;
+
+	for (; str[i] && str[i] != '\n'; ++i)
+	{
+	}
+	if (str[i])
+		++i;
+	for (; str[i]; ++i)
+	{
+		parse_line(i, ",", str, first, second);
+		if (date_first)
+			db_add_pair(first, second, map);
+		else
+			db_add_pair(second, first, map);
+	}
+	if (map.empty())
+		throw("map has no entries");
+}
+
+void db_check_conf(std::string const &str, std::string const &first, std::string const &second)
+{
+	if (str.compare(0, first.length(), first) != 0)
+		throw("unknown first key");
+	if (str[first.length()] != ',')
+		throw("unfound comma");
+	if (str.compare(first.length() + 1, second.length(), second) != 0)
+		throw("unknown second key");
+	if (str[first.length() + second.length() + 1] != '\n')
+		throw("expected at end of keys \\n");
+}
+
+void db_format(std::string const &str, std::map<std::string, float> &map)
 {
 	static std::string const date_key = "date";
 	static std::string const rate_key = "exchange_rate";
-	size_t pos_nl;
-	size_t pos_date;
-	size_t pos_rate;
-	size_t pos_comma;
+	bool date_first;
 
-	pos_comma = str.find_first_of(',');
-	if (pos_comma != std::string::npos)
-		throw ("comma not found");
-	pos_nl = str.find_first_of('\n');
-	if (pos_nl == std::string::npos)
-		throw("nl not found");
-	pos_date = str.find(date_key);
-	if (pos_date == std::string::npos || pos_nl < pos_date)
-		throw("date error");
-	pos_rate = str.find(rate_key);
-	if (pos_rate == std::string::npos || pos_nl < pos_rate)
-		throw("rate error");
-	date_first = pos_date < pos_rate;
+	if (str.empty())
+		throw("file is empty");
+	date_first = str[0] == date_key[0];
+	if (date_first)
+		db_check_conf(str, date_key, rate_key);
+	else
+		db_check_conf(str, rate_key, date_key);
+
+	db_parse(date_first, str, map);
 }
 
-void parse_line(t_format &format, std::string::const_iterator &it, std::map<std::string, float> &map)
+// INPUT
+void input_format(std::string const &str, std::map<std::string, float> const &db)
 {
+	std::istringstream is(str);
+	std::string line;
+	std::string first;
+	std::string second;
+	std::string date;
+	float value;
+	size_t i;
 
-}
-
-void	parse_db(std::string const &str, t_format &format, std::map<std::string, float> &map)
-{
-	size_t i = str.find_first_of('\n') + 1;
-
-	while (1)
+	if (str.empty())
+		throw("file argument empty");
+	getline(is, line);
+	if (is.fail())
+		throw("error getline");
+	if (line.compare("date | value") == 0)
 	{
-		if (str.compare(i, format.pre.length(), format.pre) != 0)
-			throw ("invalid pre format");
-		i += format.pre.length();
-		if (format.date_first)
-			//check_date
-		else
-			//check_other
-		if (str.compare(i, format.inter.length(), format.inter) != 0)
-			throw ("invalid inter format");
-		i += format.inter.length();
-		if (format.date_first)
-			//check_other
-		else
-			//check_date
-		if (str.compare(i, format.post.length(), format.post) != 0)
-			throw ("invalid post format");
-		i += format.post.length();
+		getline(is, line);
+		if (is.fail())
+			throw("error getLine");
+	}
+	while (line.length())
+	{
+		i = 0;
+		try {
+
+		parse_line(i, " | ", line, first, second);
+			date = parse_date(first);
+			value = parse_value(second);
+
+			if (value < 0)
+				throw("Number < 0");
+			if (1000 < value)
+				throw("Number > 1000");
+			std::map<std::string, float>::const_iterator it = db.lower_bound(first);
+			std::cout << date << ": " << (value * it->second) << std::endl;
+		}
+		catch (char const *err) {
+			std::cerr << first << " | " << second << " !> " << err << std::endl;
+		}
+		catch (const std::exception &e) {
+			std::cerr << first << " | " << second << " !> " << e.what() << std::endl;
+		}
+		getline(is, line);
+		if (is.fail())
+			throw("getline error occured");
 	}
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	/* int argc char *argv[]
-	std::string data_str;
-	if (argc != 2)
-		return 1;
-	if (read_file(argv[1], data_str))
-		return 1;
-	*/
-
 	std::string db_str;
-	bool	date_first;
 	std::map<std::string, float> db;
+	std::string input_str;
 
-	if (read_file("./bin/data.csv", db_str))
+
+	if (argc != 2) {
+		std::cerr << "Missing file argument" << std::endl;
 		return 1;
-	format_db(db_str, date_first);
-	parse_db(db_str, date_firsts, db);
+	}
+
+	if (read_file("./bin/data.csv", db_str)) {
+		std::cerr << "./bin/date.csv error while reading file" << std::endl;
+		return (1);
+	}
+
+	try {
+		db_format(db_str, db);
+	}
+	catch (const char *e) {
+		std::cerr << "DB " << e << std::endl;
+	}
+	catch (std::exception const &e) {
+		std::cerr << "DB " << e.what() << std::endl;
+	}
+
+	if (read_file(argv[1], input_str)) {
+		std::cerr << "failed to read" << std::endl;
+		return 1;
+	}
+	try {
+		input_format(input_str, db);
+	}
+	catch (const char *e)
+	{
+		std::cerr << "INPUT " << e << std::endl;
+	}
+	catch (std::exception const &e)
+	{
+		std::cerr << "INPUT " << e.what() << std::endl;
+	}
+
 	return (0);
 }
